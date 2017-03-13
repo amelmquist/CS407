6fd75576df2709210a59ca47e6cd672a00912a20
Asher Elmquist <amelmquist@wisc.edu>
2017-03-13 12:58:29 -0500

    added basic audio playing capability - currently only works with online music (http://___.mp3)

--- www/js/controllers/roomViewHostController.js
+++ www/js/controllers/roomViewHostController.js
@@ -14,4 +14,30 @@ angular.module('ionicApp').controller('roomViewHostController', function($scope,
     $state.go('roomOptionsPage');
   };
 
+
+
+  var song;
+
+  $scope.play = function(){
+    var url = "file:///www/example.mp3";
+    // var url = "http://audio.ibeat.org/content/p1rj1s/p1rj1s_-_rockGuitar.mp3";
+    // var url = "cdvfile://localhost/persistent/audio/hangouts_incoming_call.ogg"
+
+    song = new Media(url,
+      // success callback
+      function () { $scope.debug = "playAudio():Audio Success"; },
+      // error callback
+      function (err) { $scope.debug = "playAudio():Audio Error: " + err; }
+    );
+    song.play();
+
+    $scope.debug = "Playing music!";
+
+  };
+
+  $scope.pause = function(){
+    song.pause();
+    $scope.dubug = "Paused music!";
+  };
+
 });
