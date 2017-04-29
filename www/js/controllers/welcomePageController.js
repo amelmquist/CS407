7a15bcbeccf82e6a26a62b9e485c5e69407c9566
Steven Brandt <stbrandt@wisc.edu>
2017-04-29 15:34:23 -0500

    Begin adding Firebase, updated Welcome Screen Controller

--- www/js/controllers/welcomePageController.js
+++ www/js/controllers/welcomePageController.js
@@ -36,50 +36,8 @@ starter.factory('User', function() {
 
 starter.controller('welcomePageController', function(User, $rootScope, $scope, $state, $ionicViewSwitcher) {
   $scope.name = "";
-  $scope.password = "";
-  $scope.listOfUsers = {};
-
-  //TODO: load list of users from database
-
-  $scope.validateExistingName = function() {
-    // check if name and password match. If so, goToNext
-    var id = $scope.name + $scope.password;
-    if ($scope.listOfUsers[id] != null) {
-        $rootScope.currUser = $scope.listOfUsers[id];
-        $scope.goToNext();
-    }
-    else {
-      alert("Username or password is incorrect!");
-    }
-  };
-
-  $scope.validateNewName = function() {
-    // check if name has already been used, if not, goToNext. Else, display error message
-    var newName = true;
-
-    for (var userId in $scope.listOfUsers) {
-      var user = $scope.listOfUsers[userId];
-
-      if (user.name === $scope.name) {
-        newName = false;
-      }
-    }
-
-    if (newName) {
-      var id = $scope.name + $scope.password;
-      var user = new User($scope.name, $scope.password);
-      $rootScope.currUser = user;
-      $scope.listOfUsers[id] = user;
-      $scope.goToNext();
-    }
-    else {
-      alert("Username already taken!");
-    }
-  };
 
   $scope.goToNext = function() {
-    $scope.name = "";
-    $scope.password = "";
 
     $ionicViewSwitcher.nextDirection('forward');
     $state.go('routingPage');
