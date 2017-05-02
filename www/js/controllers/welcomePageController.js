7c6129bb9cb246147e9fca9a389d58b76a4fdaab
Steven Brandt <stbrandt@wisc.edu>
2017-05-02 11:49:36 -0500

    Added Form verification for Name and Room name

--- www/js/controllers/welcomePageController.js
+++ www/js/controllers/welcomePageController.js
@@ -2,8 +2,15 @@
  * Created by Steven on 3/3/2017.
  */
 
-starter.controller('welcomePageController', function($scope, $rootScope, $state, $ionicViewSwitcher) {
+starter.controller('welcomePageController', function($scope, $rootScope, $state, $ionicPlatform, $ionicViewSwitcher) {
   $scope.nameBox = "";
+
+  $ionicPlatform.ready(function () {
+    cordova.plugins.diagnostic.requestExternalStorageAuthorization(function (status) {
+    }, function (error) {
+      // $scope.perms = error;
+    });
+  });
 
   $scope.goToNext = function() {
     if($scope.nameBox == "")
